4efdf379e518ca489f99fc76b1d7ba6003942a38
Drewry Pope <dezren39@users.noreply.github.com>
2017-04-15 22:02:11 -0500

    Die Roller, No arrays

---
+++ dieRoll_dpope3/js/dieRoll_dpope3.js
@@ -0,0 +1,34 @@
+var fName;
+var numDice;
+var result = 0;
+
+fName = prompt("Please enter you first name: ");
+
+while(fName == "") {
+	fName = prompt("Reminder: Please enter you first name: ", fName);
+}
+
+numDice = prompt("How many dice would you like to roll?");
+
+while(isNaN(numDice) || parseFloat(numDice) != parseInt(numDice)) { 
+	numDice = prompt("How many dice would you like to roll?");
+}
+
+document.write("<p>Rolling " + numDice + 
+	" die...<br \>	Hey " + fName + ", you rolled a");
+
+for (i = 0; i < numDice; i++) {
+	var randomNumber = Math.floor(1 + (Math.random() * 6));
+	alert("Dice #" + (i + 1) + " " + randomNumber);
+	document.write(" " + randomNumber);
+
+	if (i + 2 == numDice) {
+		document.write(i == 0 ? " and " : ", and");
+	}
+	else if (i + 1 != numDice) {
+		document.write(",")
+	}
+	result = result + randomNumber;
+}
+
+document.write(" for a grand total of " + result + "</p>");
